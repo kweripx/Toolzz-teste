8caac5ac72d81e9ea3d383e6dea36276449e96c5
kweripx <vitor.hugoqueiroz11@gmail.com>
2023-11-24 04:06:37 -0300

    feat: added mock for repository

--- src/tests/userController.test.ts
+++ src/tests/userController.test.ts
@@ -1,21 +1,94 @@
-// UsuarioControlador.test.ts
-
-import request from 'supertest';
-import { getUserById } from '../domain/user/services/userService';
 import { UserRepository } from '../domain/user/userRepository';
 import { User } from '../domain/user/user';
+import { v4 as uuid } from 'uuid';
 
 const mockUserRepository: UserRepository = {
-  async findById(id: string): Promise<User> {
-    return { id, name: 'Test User' };
-  },
-  // TO DO other CRUD operations...
+  findById: jest.fn(),
+  create: jest.fn(),
+  getAll: jest.fn(),
+  updateUser: jest.fn(),
+  deleteUser: jest.fn(),
 };
 
-describe('getUser', () => {
-  it('fetches a user by id', async () => {
-    const user = await getUserById(mockUserRepository)(1);
-    expect(user).toEqual({ id: 1, name: 'Test User' });
+describe('User Repository', () => {
+  let userRepository: UserRepository;
+
+  beforeEach(() => {
+    userRepository = mockUserRepository;
+    jest.clearAllMocks();
   });
-  // TO DO write tests for other CRUD operations...
+
+  it('Should create a user', async () => {
+    const user: User = { id: uuid(), name: 'Test User', email: 'test@example.com' };
+    (userRepository.create as jest.Mock).mockResolvedValue(user);
+    const createdUser = await userRepository.create(user);
+    expect(createdUser).toEqual(user);
+    expect(userRepository.create).toHaveBeenCalledWith(user);
+  });
+
+  it('Should get all users', async () => {
+    const users: User[] = [
+      { id: uuid(), name: 'User 1', email: 'test@example.com' },
+      { id: uuid(), name: 'User 2', email: 'test@example.com' },
+      { id: uuid(), name: 'User 3', email: 'test@example.com' },
+    ];
+    (userRepository.getAll as jest.Mock).mockResolvedValue(users);
+    const allUsers = await userRepository.getAll();
+    expect(allUsers).toEqual(users);
+    expect(userRepository.getAll).toHaveBeenCalled();
+  });
+
+  it('Should update a user', async () => {
+    const user: User = { id: uuid(), name: 'Test User', email: 'test@example.com' };
+    (userRepository.updateUser as jest.Mock).mockResolvedValue(user);
+    const updatedUser = await userRepository.updateUser(user.id, user);
+    expect(updatedUser).toEqual(user);
+    expect(userRepository.updateUser).toHaveBeenCalledWith(user.id, user);
+  });
+
+  it('Should delete a user', async () => {
+    const userId: string = uuid();
+    (userRepository.deleteUser as jest.Mock).mockResolvedValue(null);
+    await userRepository.deleteUser(userId);
+    expect(userRepository.deleteUser).toHaveBeenCalledWith(userId);
+  });
+
+  it('Should delete multiple users', async () => {
+    const userIds: string[] = [uuid(), uuid(), uuid()];
+    (userRepository.deleteUser as jest.Mock).mockResolvedValue(null);
+
+    for (const id of userIds) {
+      await userRepository.deleteUser(id);
+    }
+
+    userIds.forEach(id => {
+      expect(userRepository.deleteUser).toHaveBeenCalledWith(id);
+    });
+  });
+
+  it('Should fail to create a user', async () => {
+    (userRepository.create as jest.Mock).mockRejectedValue(new Error('Failed to create user'));
+    await expect(userRepository.create({} as User)).rejects.toThrow('Failed to create user');
+    expect(userRepository.create).toHaveBeenCalledWith({} as User);
+  });
+
+  it('Should fail to get all users', async () => {
+    (userRepository.getAll as jest.Mock).mockRejectedValue(new Error('Failed to get users'));
+    await expect(userRepository.getAll()).rejects.toThrow('Failed to get users');
+    expect(userRepository.getAll).toHaveBeenCalled();
+  });
+
+  it('Should fail to update a user', async () => {
+    const user: User = { id: uuid(), name: 'Test User', email: 'test@example.com' };
+    (userRepository.updateUser as jest.Mock).mockRejectedValue(new Error('Failed to update user'));
+    await expect(userRepository.updateUser(user.id, user)).rejects.toThrow('Failed to update user');
+    expect(userRepository.updateUser).toHaveBeenCalledWith(user.id, user);
+  });
+
+  it('Should fail to delete a user', async () => {
+    const userId: string = uuid();
+    (userRepository.deleteUser as jest.Mock).mockRejectedValue(new Error('Failed to delete user'));
+    await expect(userRepository.deleteUser(userId)).rejects.toThrow('Failed to delete user');
+    expect(userRepository.deleteUser).toHaveBeenCalledWith(userId);
+  });
 });
