bcce0ad06b75ce53390cccaa384529ffffa502a1
kweripx <vitor.hugoqueiroz11@gmail.com>
2023-11-24 00:56:57 -0300

    fix: UUID error

--- src/infrastructure/prismaUserRepository.ts
+++ src/infrastructure/prismaUserRepository.ts
@@ -1,23 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 import { UserRepository } from '../domain/user/userRepository';
 import { User } from '../domain/user/user';
+import { UUID } from 'crypto';
 
 export const prismaUserRepository: UserRepository = {
   async create(user: User): Promise<User> {
     const newUser = await new PrismaClient().user.create({ data: user });
-    return { id: newUser.id, name: newUser.name, email: newUser.email };
+    return { id: newUser.id as UUID, name: newUser.name, email: newUser.email };
   },
   async findById(id: string): Promise<User> {
     const user = await new PrismaClient().user.findUnique({ where: { id } });
-    return { id: user.id, name: user.name, email: user.email };
+    if (!user) {
+      throw new Error('User not found');
+    }
+    return { id: user.id as UUID, name: user.name, email: user.email };
   },
   async getAll(): Promise<User[]> {
     const users = await new PrismaClient().user.findMany();
-    return users.map((user: User) => ({ id: user.id, name: user.name, email: user.email }));
+    return users.map((user) => ({
+      id: user.id as UUID,
+      name: user.name,
+      email: user.email,
+    }));
   },
   async updateUser(id: string, user: User): Promise<User> {
     const updatedUser = await new PrismaClient().user.update({ where: { id }, data: user });
-    return { id: updatedUser.id, name: updatedUser.name, email: updatedUser.email };
+    return { id: updatedUser.id as UUID, name: updatedUser.name, email: updatedUser.email };
   },
   async deleteUser(id: string): Promise<void> {
     await new PrismaClient().user.delete({ where: { id } });
